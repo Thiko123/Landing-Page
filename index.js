82963c062ba2cf5bd7625220b284298f2e31462c
Thiko123 <thikonetshiongolwe@gmail.com>
2024-06-24 17:28:36 +0200

    final landing page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function music(){
     let name= prompt("what is your name?");
-    alert("Hello,"+ name + "name check your email and follow the proceeding steps.");
+    alert("Hello, "+ name +" " +"check your email and follow the proceeding steps.");
 }
     let joinUs = document.querySelector("#button");
     joinUs.addEventListener("click", music);
